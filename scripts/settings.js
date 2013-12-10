3ace4bcd0d35afce1acafa44d80efad0d33b1453
Marco <marcos1490@gmail.com>
2013-12-10 08:58:12 -0600

    Settings info changed.

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,24 +1,24 @@
 var configuration = {
 
-  target: {
-    username: "test.chirripo@gmail.com",
-    password: "123456abC",
-    host: "imap.gmail.com", // imap host
-    port: 993, // imap port
-    secure: true, // use secure connection
-    markSeen: false, // all fetched email willbe marked as seen and not fetched next time
-  },
+    target: {
+      username: "target@gmail.com", // the email of the person you want to monitor.
+      password: "password", // the password of the email
+      host: "imap.gmail.com", // imap host
+      port: 993, // imap port
+      secure: true, // use secure connection
+      markSeen: false, // all fetched email will be marked as seen and not fetched next time
+    },
 
-  sender: {
-    username: "test.chirripo@gmail.com",
-    password: "123456abC",
-    host: "smtp.gmail.com",
-    SSL: true, // use secure connection
-  },
-  matchingPercentage: 90, // The matching percentage of words from the list
-  recipient: 'kristianva@gmail.com',
-  monitorWordList: ['drugs', 'drug', 'money', 'transaction', 'dead', 'traffic', 'marihuana', 'cocaine', 'kg', 'lsd'],
-};
+    sender: {
+      username: "someotheremail@gmail.com", // An email account to send the notification.
+      password: "some password", // The password
+      host: "smtp.gmail.com",
+      SSL: true, // use secure connection
+    },
+    matchingPercentage: 90, // The matching percentage of words from the list
+    recipient: 'recipient@gmail.com', // the email of the person that's going to be notified.
+    monitorWordList: ['drugs', 'drug', 'money', 'transaction', 'dead', 'traffic', 'marihuana', 'cocaine', 'kg', 'lsd'],
+  };
 
 
 
